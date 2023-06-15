d0080f86dcafa19f374446bc5dec7a78c037be0c
Susan Wu <susannmwu@gmail.com>
2023-06-15 16:28:12 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,85 @@
+// Alternatively, we could use this: 
+// ReactDOM.render(<hi>Hello, everyon</hi>, document.querySelector("#root"))
+
+// ReactDOM.render(<hi>Hello, everyone</hi>, document.getElementById("root"))
+
+// Try to write that 1-liner of React code again! This time, see if you can figure out how to render an <ul> with 2+ <li>s inside
+
+// ReactDOM.render(<ul><li>Hello</li><li>Cat</li></ul>, document.querySelector("#root"))
+
+// function Navbar() {
+//     return (
+//         <nav className="navbar navbar-expand-lg navbar-light bg-light">
+//             <a className="navbar-brand" href="#">Navbar</a>
+//             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+//                 <span className="navbar-toggler-icon"></span>
+//             </button>
+
+//             <div className="collapse navbar-collapse" id="navbarSupportedContent">
+//                 <ul className="navbar-nav mr-auto">
+//                 <li className="nav-item active">
+//                     <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+//                 </li>
+//                 <li className="nav-item">
+//                     <a className="nav-link" href="#">Link</a>
+//                 </li>
+//                 <li className="nav-item dropdown">
+//                     <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+//                     Dropdown
+//                     </a>
+//                     <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+//                     <a className="dropdown-item" href="#">Action</a>
+//                     <a className="dropdown-item" href="#">Another action</a>
+//                     <div className="dropdown-divider"></div>
+//                     <a className="dropdown-item" href="#">Something else here</a>
+//                     </div>
+//                 </li>
+//                 <li className="nav-item">
+//                     <a className="nav-link disabled" href="#">Disabled</a>
+//                 </li>
+//                 </ul>
+//                 <form className="form-inline my-2 my-lg-0">
+//                     <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+//                     <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+//                 </form>
+//             </div>
+//         </nav>
+//     )
+// }
+
+// function MainContent(){
+//     return (<hi>I'm learning React!</hi>)
+// }
+
+// Challenge: Create your own custom React component!
+// Call it "MainContent", and have it return a simple
+// h1 element that says "I'm learning React!"
+
+// Afterward, render it below the Navbar (which
+// you can do inside the ReactDOM.render call below)
+
+// ReactDOM.render(<div>
+//     <Navbar/>
+//     <MainContent/>
+// </div>, document.getElementById("root"))
+
+// Example to show that vanilla JS is imperative (prescribe to the program what we want executed)
+// ReactDOM.render(<h1>Hello, React!</h1>, document.getElementById("root"))
+// Challenge - recreate the line of code in vanilla JS by crerating and appending an h1 to our div #root
+// (without using innerHTML)
+// - Create a new h1 element
+// - Give it some textContent
+// - Give it a class name "header"
+// - append it as a child of the div #root
+
+// const h1 = document.createElement("h1")
+// h1.textContent = "This is an imperative way to program"
+// h1.className = "header"
+// document.getElementById("root").append(h1)
+
+// You need to give the program step-by-step instructions 
+// Create an element
+// Add some text content
+// Add a class name 
+// Append it to the DOM
+
