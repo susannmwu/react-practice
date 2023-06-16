89514f8d7b20659dc5dd81d9e1480e929e062f55
Susan Wu <susannmwu@gmail.com>
2023-06-15 18:11:11 -0700

    Completed lesson 8. JSX

--- index.js
+++ index.js
@@ -83,3 +83,70 @@
 // Add a class name 
 // Append it to the DOM
 
+
+
+// const h1 = document.createElement("h1")
+// h1.textContent = "Hello world"
+// h1.className = "header"
+// console.log(h1)
+
+// <h1 class="header">
+
+/*
+{
+    type: "h1", 
+    key: null, 
+    ref: null, 
+    props: {className: "header", children: "This is JSX"}, 
+    _owner: null, 
+    _store: {}
+}
+ */
+// This is just a JS object
+// const element = <h1 className="header">This is JSX</h1>
+// console.log(element)
+
+// JSX
+// ReactDOM.render(element, document.getElementById("root"))
+
+
+// ReactDOM.render(
+// <div>
+// <h1 className="header">This is JSX</h1>
+// <p>This is a paragraph</p>
+// </div>,
+// document.getElementById("root")
+// )
+
+//We can also write it like this
+
+// const page = (<div>
+// <h1 className="header">This is JSX</h1>
+// <p>This is a paragraph</p>
+// </div>)
+
+// console.log(page)
+
+// ReactDOM.render(page, document.getElementById("root"))
+
+// Create a navbar in JSX:
+// use the semantic `nav` element as the parent wrapper
+// have an h1 element with the brand name of your "website"
+// Insert an unordered list for the other nav elements 
+// Inside the `ul`, have three `li's` for "Pricing", 
+// "About", and "Contact"
+// Don't worry about styling yet - it'll just be plain looking HTML for now 
+
+
+// const navbar = (
+//     <nav>
+//         <h1>KittyCats</h1>
+//         <ul>
+//             <li>Pricing</li>
+//             <li>About</li>
+//             <li>Contact</li>
+//         </ul>
+//     </nav>
+// )
+
+// ReactDOM.render(navbar, document.getElementById("root"))
